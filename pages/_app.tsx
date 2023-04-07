50fca444219409ff3ff6d1eecc2b8e02990cfaae
Matthew Cochrane <matthew.cochrane.eng@gmail.com>
2023-04-08 00:58:36 +1000

    Add auth and basic tailwind templates.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from 'next/app'
+import Navbar from "@/components/navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ClerkProvider {...pageProps} >
+          <Navbar/>
+        <Component {...pageProps} />
+      </ClerkProvider>
+  )
 }
